70dfffa3ca46dcc0948cf987df229f2132156487
nyum76 <nyum2024@gmail.com>
2024-12-27 07:19:50 +0900

    ✨ Feat : signup 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view # DRF 에서는 api_view 없으면 동작 안 함
+from .serializers import SignupSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+@api_view(["POST"])
+def signup(request):
+    serializer = SignupSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "message":"회원가입이 성공적으로 완료되었습니다."
+        }, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
