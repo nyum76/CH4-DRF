cc38ba872bb3f82d197a7528f2e87b7deb966235
nyum76 <nyum2024@gmail.com>
2024-12-27 17:06:24 +0900

    📝 Comment : url 주석 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'), # 로그아웃 url path, 도전 구현
     path('profile/', views.profile, name='profile'), # 회원정보 조회 및 수정 (도전 구현)
     path('<int:user_pk>/follow/', views.follow, name='follow'), # follow 기능
-
 ]
--- spartamarket_DRF/urls.py
+++ spartamarket_DRF/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('products/', include('products.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
