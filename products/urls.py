ecdd6b654fb4f780ca09e84fbb15fe553aad7449
nyum76 <nyum2024@gmail.com>
2024-12-27 12:42:14 +0900

    ✨ Feat: 댓글 기능 추가

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.ProductListCreate.as_view(), name='article_list_create'), # 상품 등록, 조회
     path('<int:articleId>/', views.ProductDetail.as_view(), name='article_detail'), # 상품 수정, 삭제
+    path('<int:articleId>/comments/', views.CommentListCreate.as_view(), name='comments'), # 댓글 조회, 생성, 삭제 기능
+    path('<int:articleId>/comments/<int:commentId>/like/', views.CommentLike.as_view(), name='comment_like'), # 댓글 좋아요 기능
 ]
