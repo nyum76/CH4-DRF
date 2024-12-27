90494bada44194c14f0e3e43791b4c37eb38d507
nyum76 <nyum2024@gmail.com>
2024-12-27 09:57:42 +0900

    ✨ Feat: 상품 등록, 조회

--- products/views.py
+++ products/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .models import Product
+from .serializers import ProductListSerializer, ProductDetailSerializer
 
-# Create your views here.
+class ProductListCreate(APIView):
+    
+    def get(self, request): # 상품 목록 조회
+        # 1. 데이터 가져오기
+        products = Product.objects.all()
+        
+        # 2. 직렬화
+        serializer = ProductListSerializer(
+            products, many=True
+        )
+        
+        # 3. 데이터 돌려주기
+        return Response(serializer.data)
+
+    def post(self, request): # 상품 등록
+        serializer = ProductDetailSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
