d0ae29590181c74bf03cbc35d6ec3666b57b9e96
nyum76 <nyum2024@gmail.com>
2024-12-27 09:25:05 +0900

    📝 Comment : url path 주석 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'), # 로그인 url path
-    path('logout/', views.logout, name='logout'), # 로그아웃 url path
-    path('profile/', views.profile, name='profile'), # 회원정보 조회 및 수정
+    path('logout/', views.logout, name='logout'), # 로그아웃 url path, 도전 구현
+    path('profile/', views.profile, name='profile'), # 회원정보 조회 및 수정 (도전 구현)
     path('<int:user_pk>/follow/', views.follow, name='follow'), # follow 기능
 
 ]
